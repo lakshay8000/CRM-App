cdd20a840796bfc576150abce2ad28acd609a062
Lakshay Shahi <lakshay.ds@gmail.com>
2024-01-01 22:13:45 +0530

    Added line chart with multiple lines

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
-import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
+import { ArcElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip,  } from 'chart.js';
 import { useEffect } from "react";
 import React from 'react';
-import { Pie } from 'react-chartjs-2';
+import { Line, Pie } from 'react-chartjs-2';
 import { IoMdDoneAll } from "react-icons/io";
 import { MdCancel, MdEdit, MdOutlinePendingActions } from "react-icons/md";
 import { TbProgressBolt } from "react-icons/tb";
@@ -27,10 +27,10 @@ function Homepage() {
     const [ticketsState] = useTickets();
 
 
-    // for react chartjs2-
-    ChartJS.register(ArcElement, Tooltip, Legend);
+    // for react chartjs2 pie chart and line chart-
+    ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, Title);
 
-    const data = {
+    const pieChartData = {
         labels: Object.keys(ticketsState.ticketDistribution),
         datasets: [
             {
@@ -56,6 +56,97 @@ function Homepage() {
             },
         ],
     };
+
+    // for line chart-
+    const lineChartOptions = {
+        scales: {
+            y: {
+              ticks: {
+                stepSize: 1, // Set the step size for the y-axis ticks
+              },
+            },
+          },
+    };
+      
+    // this functioon will return us an obj with dates as key and their default values as 0
+    function getDateRange(range) {        
+        // get start date in the format YYYY-MM-DD. Start date= curr date - range
+        let startDate= new Date();
+        startDate.setDate(startDate.getDate() - range);
+        // console.log(startDate);
+
+        // Add all the dates from start date to curr date in an obj-
+        const dateMap= {};
+        for (let i= 0; i< range; i++) {
+            let date= startDate;
+            date.setDate(date.getDate() + 1);
+            date= date.toLocaleString().split(",")[0].split("/").reverse().join("-");
+            
+            dateMap[date] = 0;
+        }
+
+        // console.log(dateMap);
+        return dateMap;
+    }
+
+    // this function will populate the frequency map according to the ticket category passed as argument
+    function mapTicketsWithDates(category) {
+        const frequencyMap= getDateRange(15);
+        const filteredTicketList= ticketsState.ticketList.filter((ticket) => ticket.status == category);
+
+        // populate frequency map-
+        filteredTicketList.forEach((ticket) => {
+            if (
+                frequencyMap[ticket.createdAt.substring(0, 10)]
+                ||
+                frequencyMap[ticket.createdAt.substring(0, 10)] == 0
+            ) {
+                frequencyMap[ticket.createdAt.substring(0, 10)] ++ ;
+            }
+        });
+
+        // console.log(frequencyMap);
+        return frequencyMap;
+    }
+
+    const labels = Object.keys(getDateRange(15));
+
+    const lineChartData = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: Object.values(mapTicketsWithDates("open")),
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            },
+            {
+                label: 'In Progress',
+                data: mapTicketsWithDates("inProgress"),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+            {
+                label: 'Resolved',
+                data: mapTicketsWithDates("resolved"),
+                borderColor: 'rgba(255, 206, 86, 1)',
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            },
+            {
+                label: 'On Hold',
+                data: mapTicketsWithDates("onHold"),
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            },
+            {
+                label: 'Cancelled',
+                data: mapTicketsWithDates("cancelled"),
+                borderColor: 'rgba(153, 102, 255, 1)',
+                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            },
+        ],
+    };
+
 
     return (
         <HomeLayout>
@@ -134,9 +225,18 @@ function Homepage() {
 
                 </div>
 
-                <div className="charts w-[28rem] h-[28rem] mb-4">
-                    <Pie data={data} />
+                <div className='w-full flex justify-center my-16'>
+                    <div className="pie-chart w-[28rem] h-[28rem]">
+                        <Pie data={pieChartData} />
+                    </div>
                 </div>
+
+                <div className='w-full flex justify-center'>
+                    <div className='line-chart w-[60rem] mb-16'>
+                        <Line options={lineChartOptions} data={lineChartData} />
+                    </div>
+                </div>
+            
             </div>
         </HomeLayout>
     );
