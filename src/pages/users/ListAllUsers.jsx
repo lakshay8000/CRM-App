d4ea3558102c4b40c7cc63b0ba260fe0aaec6e01
Lakshay Shahi <lakshay.ds@gmail.com>
2024-01-06 16:14:39 +0530

    Added user details popup modal

--- src/pages/users/ListAllUsers.jsx
+++ src/pages/users/ListAllUsers.jsx
@@ -5,7 +5,14 @@ import { axiosInstance } from "../../config/axiosInstance";
 import HomeLayout from "../../layouts/HomeLayout";
 
 function ListAllUsers() {
-    const [userList, setUserList] = useState([]);
+    const [userList, setUserList] = useState([]);              // for showing list of all users in the table
+    const [userDisplay, setUserDisplay] = useState({           // details to be shown on click of rows
+        name: '',
+        email: '',
+        userType: '',
+        userStatus: '',
+        clientName: '',
+    });
 
     async function loadUsers() {
         const response = await axiosInstance.get("users", {
@@ -66,7 +73,12 @@ function ListAllUsers() {
 
     return (
         <HomeLayout>
-            <div className="min-h-90vh flex justify-center w-[50rem]">
+            <div className="min-h-90vh flex flex-col justify-center w-[50rem]">
+
+                <h1 className="text-center font-bold text-5xl mb-8">
+                    Users List
+                </h1>
+                
                 {
                     userList.length > 0
                     &&
@@ -74,8 +86,46 @@ function ListAllUsers() {
                         columns={columns}
                         data={userList}
                         customStyles={customStyles}
+                        onRowClicked={(row) => {
+                            // we will do functional update of state to update the state instantly-
+                            setUserDisplay(() => {
+                                return (
+                                    {
+                                        name: row.name,
+                                        email: row.email,
+                                        userType: row.userType,
+                                        userStatus: row.userStatus,
+                                        clientName: row.clientName,
+                                    }
+                                );
+                            });
+                            // for showing daisyui modal-
+                            document.getElementById('user-details-modal').showModal();
+                        }}
+                        onRowMouseEnter= {(row, event) => {
+                            event.target.style.cursor= "pointer";
+                        }}
+                        onRowMouseLeave= {(row, event) => {
+                            event.target.style.cursor= "auto";
+                        }}
                     />
                 }
+
+                {/* Daisyui Modal- */}
+                <dialog id="user-details-modal" className="modal">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-3xl"> User Details </h3>
+                        <p className="pt-8"> Name : {userDisplay.name} </p>
+                        <p className="pt-8"> Client Name : {userDisplay.clientName} </p>
+                        <p className="pt-8"> Status : {userDisplay.userStatus} </p>
+                        <p className="pt-8"> Type : {userDisplay.userType} </p>
+                        <p className="pt-8"> Email : {userDisplay.email} </p>
+                    </div>
+                    <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                    </form>
+                </dialog>
+
             </div>
         </HomeLayout>
     );
